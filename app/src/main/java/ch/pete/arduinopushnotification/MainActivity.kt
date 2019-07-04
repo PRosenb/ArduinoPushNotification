443d672d8440b4e3ad7f01ebb90d322f48bded70
Peter Rosenberg <p.rosenberg@gmx.net>
2019-07-04 18:16:38 +0200

    init

---
+++ app/src/main/java/ch/pete/arduinopushnotification/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.pete.arduinopushnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
