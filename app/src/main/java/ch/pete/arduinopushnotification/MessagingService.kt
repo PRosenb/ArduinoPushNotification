28545f0c23a4aec0a034dba957b6744853248324
Peter Rosenberg <p.rosenberg@gmx.net>
2019-07-06 23:42:43 +0200

    show push notification when in foreground

--- app/src/main/java/ch/pete/arduinopushnotification/MessagingService.kt
+++ app/src/main/java/ch/pete/arduinopushnotification/MessagingService.kt
@@ -1,9 +1,12 @@
 package ch.pete.arduinopushnotification
 
-import android.widget.Toast
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -13,6 +16,7 @@ import timber.log.Timber
 class MessagingService : FirebaseMessagingService() {
     companion object {
         const val PREF_INSTALLATION_ID = "installationId"
+        private const val CHANNEL_ID_DEFAULT = "default"
     }
 
     override fun onCreate() {
@@ -25,10 +29,8 @@ class MessagingService : FirebaseMessagingService() {
                     return@OnCompleteListener
                 }
 
-                // Get new Instance ID token
                 val token = task.result?.token
                 sendToServer(token)
-                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
             })
     }
 
@@ -39,6 +41,28 @@ class MessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         super.onMessageReceived(remoteMessage)
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID_DEFAULT)
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        remoteMessage?.notification?.title?.let { builder.setContentTitle(it) }
+        remoteMessage?.notification?.body?.let { builder.setContentText(it) }
+        builder.setAutoCancel(true)
+        builder.setContentIntent(pendingIntent)
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID_DEFAULT,
+                getString(R.string.default_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+        manager.notify(0, builder.build())
     }
 
     private fun sendToServer(token: String?) {
