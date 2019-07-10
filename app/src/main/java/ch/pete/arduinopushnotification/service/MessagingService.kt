b3bf2a4052f040d404c930b2142df28db1266e37
Peter Rosenberg <p.rosenberg@gmx.net>
2019-07-10 21:08:40 +0200

    set default notification channel id

--- app/src/main/java/ch/pete/arduinopushnotification/service/MessagingService.kt
+++ app/src/main/java/ch/pete/arduinopushnotification/service/MessagingService.kt
@@ -13,10 +13,6 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService : FirebaseMessagingService() {
-    companion object {
-        private const val CHANNEL_ID_DEFAULT = "default"
-    }
-
     override fun onNewToken(token: String?) {
         super.onNewToken(token)
         if (token != null) {
@@ -29,9 +25,10 @@ class MessagingService : FirebaseMessagingService() {
 
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val defaultChannelId = getString(R.string.default_notification_channel_id)
         val pendingIntent =
                 PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID_DEFAULT)
+        val builder = NotificationCompat.Builder(this, defaultChannelId)
         builder.setSmallIcon(R.mipmap.ic_launcher)
         remoteMessage?.notification?.title?.let { builder.setContentTitle(it) }
         remoteMessage?.notification?.body?.let { builder.setContentText(it) }
@@ -41,7 +38,7 @@ class MessagingService : FirebaseMessagingService() {
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                    CHANNEL_ID_DEFAULT,
+                    defaultChannelId,
                     getString(R.string.default_channel_name),
                     NotificationManager.IMPORTANCE_DEFAULT
             )
