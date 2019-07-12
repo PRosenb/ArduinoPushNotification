84befa64211401001b62644d3598e6378d316797
Peter Rosenberg <p.rosenberg@gmx.net>
2019-07-12 22:58:20 +0200

    rework sender script and convert it to async

--- server/lambda/send/index.js
+++ server/lambda/send/index.js
@@ -1,16 +1,15 @@
 const http = require('http');
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB();
-// const firebase = require('firebase-admin');
 
-exports.handler = (event, context, callback) => {
+exports.handler = async(event, context) => {
     var installationId = null;
     var message = null;
-    if (event.body != null) {
-        const requestBody = JSON.parse(event.body);
+    const body = event.body;
+    if (body != null) {
+        const requestBody = JSON.parse(body);
         installationId = requestBody.installationId;
         message = requestBody.message;
-        console.log(JSON.stringify(message));
     }
     // allow get parameter as well
     if (installationId == undefined || installationId == null) {
@@ -19,8 +18,7 @@ exports.handler = (event, context, callback) => {
         }
     }
     if (installationId == undefined || installationId == null) {
-        errorResponse("parameter installationId missing", callback);
-        return;
+        return getErrorResponse("parameter installationId missing");
     }
     if (message == null || message == undefined) {
         if (event.queryStringParameters != null) {
@@ -35,89 +33,39 @@ exports.handler = (event, context, callback) => {
         }
     }
     if (message == null || message == undefined) {
-        errorResponse("parameter message, title or body missing:" + event.queryStringParameters, callback);
-        return;
+        return getErrorResponse("parameter message, title or body missing: " + event.queryStringParameters);
     }
 
-    var params = {
-        TableName: 'ArduinoPushNotification',
-        Key: {
-            'InstallationId': { S: installationId },
-        },
-    };
-    ddb.getItem(params, function(err, result) {
-        if (err) {
-            console.log("DB read error", err);
-            errorResponse("DB read error", callback);
+    try {
+        var params = {
+            TableName: 'ArduinoPushNotification',
+            Key: {
+                'InstallationId': { S: installationId },
+            },
+        };
+        const dbResult = await ddb.getItem(params).promise();
+        if (dbResult.Item !== undefined && dbResult.Item !== null) {
+            var deviceToken = dbResult.Item["DeviceToken"].S;
+            return await sendPushNotification(deviceToken, message);
         }
         else {
-            if (result.Item !== undefined && result.Item !== null) {
-                var deviceToken = result.Item["DeviceToken"].S;
-                sendPushNotification(deviceToken, message, callback).then(() => {
-                    console.log("send push success");
-                }).catch((err) => {
-                    console.error(err);
-
-                    // If there is an error during processing, catch it and return
-                    // from the Lambda function successfully. Specify a 500 HTTP status
-                    // code and provide an error message in the body. This will provide a
-                    // more meaningful error response to the end client.
-                    errorResponse(err.message, callback);
-                });
-            }
-            else {
-                errorResponse("unknown deviceToken", callback);
-            }
+            return getErrorResponse("unknown deviceToken");
         }
-    });
+    }
+    catch (err) {
+        console.log("DB read error", err);
+        return getErrorResponse("DB read error");
+    }
 };
 
-function sendResponse(callback, body) {
-    callback(null, {
-        statusCode: 201,
-        body: body,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
-}
+async function sendPushNotification(deviceToken, message) {
+    try {
+        message.to = deviceToken;
+        if (message.priority == undefined) {
+            message.priority = "high";
+        }
 
-function errorResponse(errorMessage, callback) {
-    callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-            Error: errorMessage,
-        }),
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
-}
-
-// function sendPushNotificationNew() {
-// var message = {
-//   data: {
-//     score: '850',
-//     time: '2:45'
-//   },
-//   token: "dyoo-g45DbE:APA91bGx6NWpjqp9Min5O3GAkyO3FGcJSF-cKNtEQSiaxX60-DFzOIuYfpeSTQd_Hwhp-Bg7MWbDY707STpJdgWqrSS6NvsFrSX9aeD8uupql9cMgfbFJX9h0bBpHeEQvx0PRjMCzDt6"
-// };
-
-// // Send a message to the device corresponding to the provided
-// // registration token.
-// firebase.messaging().send(message)
-//   .then((response) => {
-//     // Response is a message ID string.
-//     console.log('Successfully sent message:', response);
-//   })
-//   .catch((error) => {
-//     console.log('Error sending message:', error);
-//   });
-// }
-
-function sendPushNotification(deviceToken, message, callback) {
-    return new Promise((resolve, reject) => {
-        const options = {
+        const httpOptions = {
             host: "fcm.googleapis.com",
             path: "/fcm/send",
             method: 'POST',
@@ -126,52 +74,80 @@ function sendPushNotification(deviceToken, message, callback) {
                 'Content-Type': 'application/json'
             }
         };
+        const postBodyJson = JSON.stringify(message);
+        const response = await sendHttpRequest(httpOptions, postBodyJson);
 
-        const httpCallback = function(response) {
-            var data = "";
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-            response.on('end', () => {
-                console.log("data: ", data);
-                var parsedData = JSON.parse(data);
-                if (parsedData.total_pages !== null) {
-                    console.log("success: ", parsedData.success);
-                    console.log("message_id: ", parsedData.results[0].message_id);
-                    if (parsedData.success == 1) {
-                        sendResponse(callback, JSON.stringify({
-                            success: parsedData.success,
-                            message_id: parsedData.results[0].message_id
-                        }));
-                    }
-                    else {
-                        var error = parsedData.results[0].error;
-                        if (error == null || error == undefined) {
-                            error = "unknown error";
-                        }
-                        errorResponse(error, callback);
-                    }
+        var parsedData = JSON.parse(response);
+        if (parsedData.total_pages !== null) {
+            console.log("success: ", parsedData.success);
+            console.log("message_id: ", parsedData.results[0].message_id);
+            if (parsedData.success == 1) {
+                return {
+                    statusCode: 201,
+                    body: JSON.stringify({
+                        success: parsedData.success,
+                        message_id: parsedData.results[0].message_id
+                    }),
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    },
+                };
+            }
+            else {
+                var error = parsedData.results[0].error;
+                if (error == null || error == undefined) {
+                    error = "unknown error";
                 }
-                else {
-                    sendResponse(callback, JSON.stringify({
-                        error: "unknown response"
-                    }));
-                }
-            });
-
-            response.on('error', (e) => {
-                reject(e.message);
-            });
+                return getErrorResponse(error);
+            }
         }
-        var req = http.request(options, httpCallback);
-
-        message.to = deviceToken;
-        if (message.priority == undefined) {
-            message.priority = "high";
+        else {
+            return getErrorResponse("unknown response");
         }
-        const messageJson = JSON.stringify(message);
-        console.log("message: ", messageJson);
-        req.write(messageJson);
-        req.end();
+    }
+    catch (err) {
+        console.log("send push notification error", err);
+        return getErrorResponse("send push notification error");
+    }
+}
+
+function sendHttpRequest(options, body) {
+    return new Promise((resolve, reject) => {
+        try {
+            const httpCallback = function(response) {
+                var data = "";
+                response.on('data', (chunk) => {
+                    data += chunk;
+                });
+                response.on('end', () => {
+                    resolve(data);
+                });
+                response.on('error', (err) => {
+                    console.log("error sendHttpPostRequest", err);
+                    reject(err);
+                });
+            };
+            const request = http.request(options, httpCallback);
+            if (body != undefined && body != null) {
+                request.write(body);
+            }
+            request.end();
+        }
+        catch (err) {
+            console.log("error sendHttpPostRequest", err);
+            reject(err);
+        }
     });
+}
+
+function getErrorResponse(errorMessage) {
+    return {
+        statusCode: 500,
+        body: JSON.stringify({
+            error: errorMessage,
+        }),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    };
 }
